e0f1358deeeae5befb6f8ec4b9cc8b6e8b81e587
darrelhong <darrel.hong@gmail.com>
2023-05-04 09:23:32 +0800

    chore: add prettier-plugin-tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="container mx-auto prose">
+<div class="container prose mx-auto">
   <h1>Welcome to SvelteKit</h1>
   <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </div>
