784c359ea7886e586e2cd071f85df245a13074cb
darrelhong <darrel.hong@gmail.com>
2023-05-05 09:33:55 +0800

    feat: add daisyui and default theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,21 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        pop: {
+          'color-scheme': 'light',
+          primary: '#fcb928',
+          secondary: '#7b5ea7',
+          accent: '#f38ba3',
+          neutral: '#12b4e5',
+          'base-100': '#ffffff',
+          '--rounded-btn': '1.9rem',
+          '--btn-text-case': 'none'
+        }
+      }
+    ]
+  }
 } satisfies Config;
