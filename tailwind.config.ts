f5df7e610e197681b3ef4acf69a6ff2f340e9705
darrelhong <darrel.hong@gmail.com>
2023-05-05 10:00:31 +0800

    feat: setup dark theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -11,15 +12,26 @@ export default {
       {
         pop: {
           'color-scheme': 'light',
-          primary: '#fcb928',
+          primary: '#fbbf24',
           secondary: '#7b5ea7',
           accent: '#f38ba3',
           neutral: '#12b4e5',
           'base-100': '#ffffff',
           '--rounded-btn': '1.9rem',
           '--btn-text-case': 'none'
+        },
+        dark: {
+          'color-scheme': 'dark',
+          primary: '#fcb928',
+          secondary: '#7b5ea7',
+          accent: '#f38ba3',
+          neutral: '#12b4e5',
+          'base-100': colors.neutral[800],
+          '--rounded-btn': '1.9rem',
+          '--btn-text-case': 'none'
         }
       }
     ]
-  }
+  },
+  darkMode: ['class', '[data-theme="dark"]']
 } satisfies Config;
