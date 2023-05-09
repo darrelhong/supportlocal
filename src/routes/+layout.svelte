1bc9fe5dd8cd7c2a5bc6e0922c5844a280a41bb7
darrelhong <darrel.hong@gmail.com>
2023-05-09 16:13:53 +0800

    fix: use border instead of shadow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <header
-  class="min-h-12 sticky top-0 flex items-center bg-base-100 px-2 py-1 shadow dark:shadow-neutral-900/60"
+  class="min-h-12 sticky top-0 flex items-center border-b-2 border-black bg-base-100 px-2 py-1 dark:border-white"
 >
   <div class="container mx-auto my-auto flex items-center gap-2">
     <a href="/" class="text-2xl font-bold text-red-500">#supportlocal</a>
