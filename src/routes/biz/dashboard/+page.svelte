62dc28c387b128fe30227c686e12f6dfba259c77
darrelhong <darrel.hong@gmail.com>
2023-05-11 22:39:16 +0800

    feat: remove query param after signup

--- src/routes/biz/dashboard/+page.svelte
+++ src/routes/biz/dashboard/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import { onMount } from 'svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
   $: ({ session } = data);
+  $: ({ user } = session);
+
+  onMount(() => {
+    const params = $page.url.searchParams;
+    params.delete('signup');
+    history.replaceState(
+      null,
+      '',
+      `${$page.url.pathname}${params.toString() && `?${params.toString()}`}`
+    );
+  });
 </script>
 
 <h3>{`${session?.user.email}'s dashboard`}</h3>
