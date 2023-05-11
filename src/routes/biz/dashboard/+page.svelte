dc910fa5155ca8536a5589ad0e63025f62aae326
darrelhong <darrel.hong@gmail.com>
2023-05-11 22:40:09 +0800

    feat: inital layout with mock dashboard listings

--- src/routes/biz/dashboard/+page.svelte
+++ src/routes/biz/dashboard/+page.svelte
@@ -19,7 +19,34 @@
   });
 </script>
 
-<h3>{`${session?.user.email}'s dashboard`}</h3>
+<h3>{`${user.email}'s dashboard`}</h3>
+
 {#if $page.url.searchParams.get('signup')}
   <p class="text-sm text-success">Successfully verified email</p>
 {/if}
+
+<div class="flex items-center justify-between p-2">
+  <h2 class="m-0">Your listings</h2>
+  <a href="dashboard/create" class="btn-neutral btn">Create new listing</a>
+</div>
+
+<div class="grid gap-2 sm:grid-cols-2">
+  {#each Array(10).fill(0) as _, i}
+    <div
+      class="overflow-hidden rounded-md border-2 border-black pb-2 prose-img:m-0 dark:border-white sm:border-2"
+    >
+      <img
+        src="https://picsum.photos/seed/${i}/400/300"
+        alt="Listing"
+        class="max-h-48 w-full object-cover"
+      />
+      <div class="px-2 pt-2 prose-h3:m-0 prose-p:leading-5">
+        <h3 class="text-xl font-semibold">Listing title</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        <div class="flex justify-end">
+          <a href="edit" class="btn-accent btn-sm btn">Edit</a>
+        </div>
+      </div>
+    </div>
+  {/each}
+</div>
