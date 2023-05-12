2e2a9bc557dd2cba298055b4ec2e1d053123236e
darrelhong <darrel.hong@gmail.com>
2023-05-12 11:33:44 +0800

    feat: protect /biz/dashboard routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 
@@ -8,6 +8,14 @@ export const handle = (async ({ event, resolve }) => {
     supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
     event
   });
+
+  // protect biz/dashboard routes
+  if (event.url.pathname.startsWith('/biz/dashboard')) {
+    const session = (await event.locals.supabase.auth.getSession()).data.session;
+    if (!session) {
+      throw redirect(303, '/biz/login');
+    }
+  }
 
   const cookieTheme = event.cookies.get('theme');
 
