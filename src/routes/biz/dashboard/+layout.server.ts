0f582a6a5ffb84d83136f5afbd2991ebeeba599e
darrelhong <darrel.hong@gmail.com>
2023-05-12 12:07:01 +0800

    feat: dashboard routes session not null

---
+++ src/routes/biz/dashboard/+layout.server.ts
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ parent }) => {
+  const { session } = await parent();
+
+  if (!session) {
+    throw redirect(303, '/biz/login');
+  }
+
+  return { session };
+};
